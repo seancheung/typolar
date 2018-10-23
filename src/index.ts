37f1b8f6b4f9a173752c4af89490640a0fe58855
Sean Cheung <theoxuanx@gmail.com>
2018-10-24 02:38:31 +0800

    added entrty file

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,13 @@
-import { createServer } from 'http'
-import app from './app'
+export * from './controller'
+export * from './model'
+export * from './service'
 import config from './config'
+import * as errors from './errors'
+import * as guards from './guards'
+import * as json from './json'
 import getLogger from './logger'
-import { resolveHost } from './misc'
-import { isDevMode } from './utils'
-
-const server = createServer(app)
-const logger = getLogger('server')
-
-server.on('error', err => {
-    const error = err as any
-    if (error.syscall === 'listen') {
-        const bind =
-            typeof config.app.port === 'string'
-                ? 'Pipe ' + config.app.port
-                : 'Port ' + config.app.port
-
-        // handle specific listen errors with friendly messages
-        switch (error.code) {
-            case 'EACCES':
-                logger.fatal(`${bind} requires elevated privileges`)
-                break
-            case 'EADDRINUSE':
-                logger.fatal(`${bind} is already in use`)
-                break
-            default:
-                logger.fatal(err)
-                break
-        }
-    } else {
-        logger.fatal(err)
-    }
-
-    process.exit(1)
-})
-
-server.on('listening', () => {
-    const addr = server.address()
-    const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port
-    if (isDevMode()) {
-        resolveHost(config.app.host, config.app.port)
-    } else {
-        logger.info('Listening on ' + bind)
-    }
-})
-
-server.listen(config.app.port, config.app.host)
-
-export = server
+import * as types from './types'
+import * as utils from './utils'
+export { config, errors, guards, json, getLogger, utils, types }
+export { default as app } from './app'
+export { default as server } from './server'
